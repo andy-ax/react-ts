5c52e178e374f2474671cdac88bad00672496c27
andy-mars <andy9887776666@gmail.com>
2021-06-08 18:04:08 +0800

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, { Children } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type arg = {
+  children: string;
+  title: string;
+}
+
+const H3 = ({children, title}: arg) => {
+  return <h3 title={title}>{children}</h3>
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <H3 title="name">hello</H3>
       </header>
     </div>
   );
 }
+
 
 export default App;
